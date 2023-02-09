351ab0d55e3fe0bf32374cecfde169bd4d9371b5
ishrafil2233 <ishrafil2233@gmail.com>
2023-02-09 20:29:09 +0600

    add connected code

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y6gag.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// to check database connected or not 
+client.connect()
+    .then(result => {
+        console.log('mongoDb connected')
+    })
+    .catch(error => {
+        console.log('mongoDb not connected')
+    })
+
 
 async function run() {
     try {
